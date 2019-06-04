0836fff9612ebf7840d0f2b33909495b7c7a84fd
Isaac Sunoo <isaacsunoo@gmail.com>
2019-06-04 16:20:52 -0600

    added header to homepage of application

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class='header'>
+    <h1>Photo Searcher</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped>
+  .header {
+    background: #000;
+    color: #FFF;
+    text-align: center;
+    padding: 10px;
+  }
+</style>
