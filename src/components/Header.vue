9d0d12af84cce4a39b58bcab8b3ae909bea78314
Isaac Sunoo <isaacsunoo@gmail.com>
2019-06-04 21:00:22 -0600

    added sticky header to application

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,32 @@
 <template>
-  <header class='header'>
-    <h1>Photo Searcher</h1>
-  </header>
+  <FixedHeader>
+    <header class='navbar'>
+      <h1>Photo Searcher</h1>
+    </header>
+  </FixedHeader>
 </template>
 
 <script>
+import FixedHeader from 'vue-fixed-header';
 export default {
-  name: 'Header'
+  name: 'Header',
+  components: {
+    FixedHeader
+  }
 }
 </script>
 
 <style scoped>
-  .header {
+  .navbar {
     background: #000;
     color: #FFF;
     text-align: center;
     padding: 10px;
   }
+  .navbar.vue-fixed-header--isFixed {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+}
 </style>
